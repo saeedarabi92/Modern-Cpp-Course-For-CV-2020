6d3518d803f908be0c6f7f8e7c6a3e43d9a58744
saeedarabi92 <arabi@iastate.edu>
2020-12-30 23:08:00 -0600

    Add header to the files

--- homework_3/src/html_writer.cpp
+++ homework_3/src/html_writer.cpp
@@ -1,3 +1,9 @@
+/**********************************
+ * @ Author: Saeed Arabi
+ * @ Create Time: 2020-12-30 22:47:50
+ * @ Email: arabi@iastate.edu
+ **********************************/
+
 #include "html_writer.hpp"
 
 #include <fstream>
