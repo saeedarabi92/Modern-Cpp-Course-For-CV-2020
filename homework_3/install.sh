570a3588b0a50c04dfe46a8bf94064f69549cc19
saeedarabi92 <arabi@iastate.edu>
2021-01-01 23:18:40 -0600

    Update install.sh

--- homework_3/install.sh
+++ homework_3/install.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 rm -rf build
+rm -rf bin
 mkdir build
 cd build
 cmake ..
 make install
 cd ../bin/
+rm ../web_app/Saeed_html_file.html
 ./main > Saeed_html_file.html
-mv Saeed_html_file.html web_app
-# open ../web_app/Saeed_html_file.html
+mv Saeed_html_file.html ../web_app/
+open ../web_app/Saeed_html_file.html
